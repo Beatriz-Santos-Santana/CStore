30948ec05b58f07b417b4976c1b10e74d2ec9998
Beatriz Santos Santana <beatriz.ssantana14@senacsp.edu.br>
2024-10-27 21:15:55 -0300

    adicionando até o lab8 com erro

---
+++ carstore/model/User.java
@@ -0,0 +1,21 @@
+package br.com.carstore.model;
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(String password, String username) {
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
